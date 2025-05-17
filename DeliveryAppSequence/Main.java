f333d576e11d8e8dcee0a1c2c545909d7eb51511
skucomputer0830 <hyse5560@naver.com>
2025-05-17 13:30:24 +0900

    Add files via upload

---
+++ DeliveryAppSequence/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        User user = new User();
+        App app = new App();
+        user.placeOrder(app, "Kimchi Fried Rice");
+    }
+}
