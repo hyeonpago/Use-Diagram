bd5e3dea751c9504494e80d0f9b017c04a954bd0
skucomputer0830 <hyse5560@naver.com>
2025-05-24 20:22:40 +0900

    Delete DeliveryAppSequence directory

--- DeliveryAppSequence/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        User user = new User();
-        App app = new App();
-        user.placeOrder(app, "Kimchi Fried Rice");
-    }
-}
